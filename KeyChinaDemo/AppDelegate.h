e9afec6af1b3292d8fc7891676f649d95ae7940e
Miao123 <miao_ios@163.comc>
2017-08-14 20:32:16 +0800

    Initial Commit

---
+++ KeyChinaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyChinaDemo
+//
+//  Created by 苗建浩 on 2017/8/14.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
